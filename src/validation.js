02c7dee24c95b9b01ce4a6fd36f0cc789c372861
Marco <tignanellimarco@gmail.com>
2023-06-14 16:46:04 -0300

    documentation

--- src/validation.js
+++ src/validation.js
@@ -1,5 +1,6 @@
 module.exports = (req, res, next) => {
     const { id } = req.params
+    // If the id passed is valid, continue, else, end request
     if (Number(id)) return next()
     else return res.status(400).json({ code: 400, error: "Should pass a valid flight id" })
 }
