e21b0e8e6c906502b0d119e2f84a6ef65bae6f22
Marco <tignanellimarco@gmail.com>
2023-06-14 15:45:01 -0300

    cleaning passengers from flight data

---
+++ cleanPassengers.js
@@ -0,0 +1,16 @@
+module.exports = cleanPassengers = (boarding_passes) => {
+    return boarding_passes.map(bp => {
+        return {
+            passengerId: bp.passenger.passenger_id,
+            dni: bp.passenger.dni,
+            name: bp.passenger.name,
+            age: bp.passenger.age,
+            country: bp.passenger.country,
+            boardingPassId: bp.boarding_pass_id,
+            purchaseId: bp.purchase_id,
+            seatTypeId: bp.seat_type_id,
+            seatId: bp.seat_id
+        }
+    })
+
+}
