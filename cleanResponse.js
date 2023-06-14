c2a3b533615725d9d920683b0d804674f124315c
Marco <tignanellimarco@gmail.com>
2023-06-14 15:45:12 -0300

    cleaning response

---
+++ cleanResponse.js
@@ -0,0 +1,14 @@
+module.exports = cleanResponse = (flight, passengers) => {
+    return {
+        code: 200,
+        data: {
+            flightId: flight.flight_id,
+            takeoffDateTime: flight.takeoff_date_time,
+            takeoffAirport: flight.takeoff_airport,
+            landingDateTime: flight.landing_date_time,
+            landingAirport: flight.landing_airport,
+            airplaneId: flight.airplane_id,
+            passengers: [...passengers]
+        }
+    }
+}
