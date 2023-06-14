91d74937a8244edd870058fb594bca8bc016efab
Marco <tignanellimarco@gmail.com>
2023-06-14 16:26:19 -0300

    db setup and associations

---
+++ db.js
@@ -0,0 +1,45 @@
+// Conexion a la db
+const fs = require('fs');
+const path = require('path');
+const { Sequelize, DataTypes } = require('sequelize')
+const {
+    DB_USERNAME, DB_PASSWORD, DB_HOST, DB_NAME
+} = process.env;
+
+const sequelize = new Sequelize(DB_NAME, DB_USERNAME, DB_PASSWORD, {
+    host: DB_HOST,
+    dialect: "mysql"
+});
+
+const basename = path.basename(__filename);
+const modelDefiners = [];
+
+// Leemos todos los archivos de la carpeta Models, los requerimos y agregamos al arreglo modelDefiners
+fs.readdirSync(path.join(__dirname, '/models'))
+    .filter((file) => (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js'))
+    .forEach((file) => {
+        modelDefiners.push(require(path.join(__dirname, '/models', file)));
+    });
+
+// Injectamos la conexion (sequelize) a todos los modelos
+modelDefiners.forEach(model => model(sequelize, DataTypes));
+
+// Capitalizamos los nombres de los modelos ie: product => Product
+let entries = Object.entries(sequelize.models);
+let capsEntries = entries.map((entry) => [entry[0][0].toUpperCase() + entry[0].slice(1), entry[1]]);
+sequelize.models = Object.fromEntries(capsEntries);
+
+// Deestructuramos los modelos
+const { Flight, Boarding_pass, Passenger } = sequelize.models;
+// Relacionamos los modelos
+Flight.hasMany(Boarding_pass, {
+    foreignKey: "flight_id",
+})
+Boarding_pass.belongsTo(Passenger, {
+    foreignKey: "passenger_id",
+})
+
+module.exports = {
+    ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
+    conn: sequelize,     // para importart la conexión { conn } = require('./db.js');
+};
