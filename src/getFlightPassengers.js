e2b8549f596febee975b6bad9d75c32d0fcce7d8
Marco <tignanellimarco@gmail.com>
2023-06-14 18:20:57 -0300

    airplane passed as params

--- src/getFlightPassengers.js
+++ src/getFlightPassengers.js
@@ -20,6 +20,7 @@ module.exports = async (req, res) => {
 
         res.status(200).json(response)
     } catch (error) {
+        console.log(error);
         res.status(400).json({ error })
     }
 }
